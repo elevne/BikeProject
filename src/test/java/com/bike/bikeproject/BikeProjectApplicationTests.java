92db16ec6249c5206c3db1acc14e765a9548598c
elevne <cwi5525@naver.com>
2023-07-31 10:48:03 +0900

    First commit

---
+++ src/test/java/com/bike/bikeproject/BikeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bike.bikeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
