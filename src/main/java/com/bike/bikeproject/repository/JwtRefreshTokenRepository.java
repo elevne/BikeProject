7bbf5d12e26cc0df6d24edd199b90f0a2095bf0b
elevne <cwi5525@naver.com>
2023-08-15 16:09:07 +0900

    JwtUtil 작업

--- src/main/java/com/bike/bikeproject/repository/JwtRefreshTokenRepository.java
+++ src/main/java/com/bike/bikeproject/repository/JwtRefreshTokenRepository.java
@@ -4,7 +4,11 @@ import com.bike.bikeproject.entity.JwtRefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JwtRefreshTokenRepository extends JpaRepository<JwtRefreshToken, Long> {
+
+    Optional<JwtRefreshToken> findByToken(String token);
 
 }
